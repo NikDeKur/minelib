2b23a23817d57eb892c0b44fa860364f5f5f76ec
NikDeKur <125550021+NikDeKur@users.noreply.github.com>
2024-07-31 21:55:22 +0100

    RPG DamageSources classes moved

--- src/main/kotlin/dev/nikdekur/minelib/rpg/combat/DamageSource.kt
+++ src/main/kotlin/dev/nikdekur/minelib/rpg/combat/DamageSource.kt
@@ -1,5 +1,6 @@
 package dev.nikdekur.minelib.rpg.combat
 
+import dev.nikdekur.minelib.rpg.profile.RPGProfile
 import org.bukkit.World
 
 interface DamageSource {
@@ -14,12 +15,42 @@ interface DamageSource {
         const val CUSTOM = "CUSTOM"
     }
 
+    interface RPG : DamageSource {
+        val attacker: RPGProfile
+    }
+
+    class Fire(
+        override val world: World,
+        val count: Int
+    ) : DamageSource {
+        override val type = Type.FIRE
+    }
+
+    class Drown(
+        override val world: World,
+        val depth: Int
+    ) : DamageSource {
+        override val type = Type.DROWN
+    }
+
+    class Fall(
+        override val world: World,
+        val height: Int
+    ) : DamageSource {
+        override val type = Type.FALL
+    }
+
+    class Unknown(
+        override val world: World
+    ) : DamageSource {
+        override val type = Type.CUSTOM
+    }
 
 
     companion object {
-        fun fire(world: World, count: Int) = FireDamageSource(world, count)
-        fun drown(world: World, depth: Int) = DrownDamageSource(world, depth)
-        fun fall(world: World, height: Int) = FallDamageSource(world, height)
-        fun custom(world: World) = CustomDamageSource(world)
+        fun fire(world: World, count: Int) = Fire(world, count)
+        fun drown(world: World, depth: Int) = Drown(world, depth)
+        fun fall(world: World, height: Int) = Fall(world, height)
+        fun unknown(world: World) = Unknown(world)
     }
 }
